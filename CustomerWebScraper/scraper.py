f6770fc5df73213e131158c2539bd1a4f1da13f6
Teodor Agavriloai <teoagav@gmail.com>
2019-09-14 17:02:32 -0400

    Database population for customer data finished

--- CustomerWebScraper/scraper.py
+++
@@ -1,6 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-url =
-reponse = requests.get(url, timeout=5)
-contnent = BeautifulSoup(response.content, 'html.parser')
--- app/app.py
+++ app/app.py
@@ -1,15 +1,23 @@
 from flask import Flask
 import requests
+import json
 app = Flask(__name__)
 
 
 @app.route('/hello')
 def hello():
-    response = requests.get('https://api.td-davinci.com/api/branches',
-    headers = { 'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3' +
-    'MiOiJDQlAiLCJ0ZWFtX2lkIjoiZDQyMTg0OWYtODBhMi0zMjZiLWE5YzgtZmI4Zjk2MDM3OT' +
-    'VhIiwiZXhwIjo5MjIzMzcyMDM2ODU0Nzc1LCJhcHBfaWQiOiI4MWEzMjc2MC03NTdhLTRlND' +
-    'AtYmE4ZS04N2EyNDIwNTY3OWEifQ.6_x7VPFdBooncl89XT30nA7_UYMMCUrrUiLl3tYmwpE' }
+    response = requests.post('https://api.td-davinci.com/api/simulants/search',
+        headers = { 'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.ey' +
+        'Jpc3MiOiJDQlAiLCJ0ZWFtX2lkIjoiZDQyMTg0OWYtODBhMi0zMjZiLWE5YzgtZmI4Zj' +
+        'k2MDM3OTVhIiwiZXhwIjo5MjIzMzcyMDM2ODU0Nzc1LCJhcHBfaWQiOiI4MWEzMjc2MC' +
+        '03NTdhLTRlNDAtYmE4ZS04N2EyNDIwNTY3OWEifQ.6_x7VPFdBooncl89XT30nA7_UYM' +
+        'MCUrrUiLl3tYmwpE'},
+        json = {
+            "continuationToken":"",
+            "searchCriteria": [
+                {"key":"age","operation":">","value":25},
+                {"key":"age","operation":"<","value":34}
+            ]}
     )
     return response.json()
 
